19306a84e68c2b391c4fec9160f173ab1d1943b4
sunilkumarjelly11-ui <sunilkumarjelly11@gmail.com>
2026-09-07 14:19:54 +0000

    Fix Lift Technician app

---
+++ app/src/main/java/io/github/sunilkumarjelly11_ui/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package io.github.sunilkumarjelly11_ui.twa;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
