47bb7806b219ac2abb2a875d03cb13f5ab417852
sunilkumarjelly11-ui <sunilkumarjelly11@gmail.com>
2026-09-17 18:09:51 +0530

    Update service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',e=>e.waitUntil(caches.open('lift-tech-v2').then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE_NAME='lift-tech-v3';self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./manifest.json'])))});self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
